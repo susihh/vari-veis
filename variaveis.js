75fd0119a59aef69d738e94f85695377d2232087
susihh <susana.tuasco@escola.pr.gov.br>
2026-02-26 09:52:19 -0300

    Add files via upload

---
+++ variaveis.js
@@ -0,0 +1,43 @@
+let a = 40;
+let b = 10;
+let c = 20;
+let total = 0;
+let totalde = 0;
+let x = 5;
+total = ( a + b + c );
+totalde = ( a + b + c ) / x;
+console.log("o valor do total é:", total);
+console.log("o valor do totalde é:", totalde); 
+console.log(outraconta ());
+console.log(media ());
+
+
+// inicio da função
+function outraconta(){
+    var d = 10;
+    var e = 20;
+    var f = 30;
+    var tot = 0; 
+    var xx = 100;
+    var totde = 0;
+    tot = ( d + e + f );
+    totde = ( d + e + f ) / xx;
+    //console.log (" o valor de tot é:", tot); 
+    //console.log (" o valor de totde é:", totde); 
+    return tot;
+    return totde;
+
+}
+
+
+
+
+function media () {
+    let nfinal = 0;
+    n1 = 30;
+    n2 = 90;
+    n3 = 100;
+    nfinal = ( n1 + n2 + n3 ) / 3;
+    //console.log (" a media é", nfinal.toFixed(1));
+    return nfinal.toFixed(1);
+    }
